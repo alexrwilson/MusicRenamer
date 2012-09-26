912a3373640d07f93afc71ecd59daf6ef714d4e4
= <c0m47053@gmail.com>
2012-09-26 12:56:55 +0100

    Now renames successfully, regex to match already numbered tracks not working yet, so renames indiscriminately

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 MusicRenamer
 ============
 
-Renames music to have a leading track number for dumb usb mp3 players that don't read ID tags properly
+Renames music to have a leading track number for dumb usb MP3 players that don't read ID tags properly.
+
+Currently only looks at MP3 files, since this is the only format I have found players for that experience this problem.
+
+Uses mutagen 1.20 for tag retrieval.
--- renamer.py
+++ renamer.py
@@ -3,19 +3,44 @@
 import fnmatch
 import os
 import argparse
+import re
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
 
 parser = argparse.ArgumentParser(description='Renames MP3 files to prepend trach numbers.')
 parser.add_argument("dir", help="directory to search for music files")
 
 args = parser.parse_args()
 
+track_number_pattern = re.compile('\d+')
 
-def add_track_number(filename):
-	print "Adding Track number to ", filename
+def add_track_number(full_filename, filename):
+	audio_file = MP3(full_filename, ID3=EasyID3)
+	raw_track_number_string = audio_file["tracknumber"][0]
+
+
+	match = track_number_pattern.match(raw_track_number_string)
+	if match:
+		track_number = match.group()
+		current_track_number_pattern = re.compile('$0*' + track_number) # Needs some work
+		filename_match = current_track_number_pattern.match(filename)
+		if not filename_match:
+			processed_filename = track_number.zfill(3) + " - " + filename
+			processed_full_filename = full_filename.replace(filename, processed_filename)
+			os.rename(full_filename, processed_full_filename)
+
+	else:
+		print"WARN: No track number found for ", full_filename
 
 
 rootdir = '/'
 for root, subFolders, files in os.walk(args.dir):
 	for file in files:
-		if fnmatch.fnmatch(file, '*py'):
-			add_track_number(os.path.join(root, file))
+		if fnmatch.fnmatch(file, '*mp3'):
+			#print root
+			add_track_number(os.path.join(root, file), file)
+
+
+
+
+#print EasyID3.valid_keys.keys()
