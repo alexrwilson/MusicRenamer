2b339071b4a1c2bf03996e1b35de510fabaebb8f
= <c0m47053@gmail.com>
2012-09-26 15:22:59 +0100

    All working, updated readme with usage instructions

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 MusicRenamer
 ============
 
-Renames music to have a leading track number for dumb usb MP3 players that don't read ID tags properly.
+Renames music to have a leading track number for dumb USB MP3 players that don't read ID tags properly.
 
 Currently only looks at MP3 files, since this is the only format I have found players for that experience this problem.
 
-Uses mutagen 1.20 for tag retrieval.
+Uses [Mutagen](http://code.google.com/p/mutagen/) 1.20 for tag retrieval.
+
+
+#Usage
+
+To run, you need Python 2 (developed against 2.7.3 on Linux), and the Mutagen audio metadata library.
+
+The easiest way to set up dependencies is using [pip](http://www.pip-installer.org)
+
+```console
+pip install mutagen
+```
+
+Alternatively, you can get a release from the (Mutagen site)[http://code.google.com/p/mutagen/downloads/list] for manual installation.
+
+Once installed, running renamer.py through a python interpreter with the path to the root directory as an argument should cause all MP3 files to be renamed with a leading track title.
+
--- renamer.py
+++ renamer.py
@@ -22,25 +22,23 @@ def add_track_number(full_filename, filename):
 	match = track_number_pattern.match(raw_track_number_string)
 	if match:
 		track_number = match.group()
-		current_track_number_pattern = re.compile('$0*' + track_number) # Needs some work
+		current_track_number_pattern = re.compile('0*' + track_number) # Needs some work
 		filename_match = current_track_number_pattern.match(filename)
 		if not filename_match:
 			processed_filename = track_number.zfill(3) + " - " + filename
 			processed_full_filename = full_filename.replace(filename, processed_filename)
 			os.rename(full_filename, processed_full_filename)
-
 	else:
 		print"WARN: No track number found for ", full_filename
 
-
-rootdir = '/'
-for root, subFolders, files in os.walk(args.dir):
-	for file in files:
-		if fnmatch.fnmatch(file, '*mp3'):
-			#print root
-			add_track_number(os.path.join(root, file), file)
-
+if __name__ == '__main__':
+	for root, subFolders, files in os.walk(args.dir):
+		for file in files:
+			if fnmatch.fnmatch(file, '*mp3'):
+				#print root
+				add_track_number(os.path.join(root, file), file)
 
 
 
-#print EasyID3.valid_keys.keys()
+
+
